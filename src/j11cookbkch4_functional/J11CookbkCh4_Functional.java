92f467b5cefb5b38d0fbb27c6890a294812ba9ab
Ken Ng <ng.ken84@gmail.com>
2018-12-13 09:40:46 -0800

    first commit

---
+++ src/j11cookbkch4_functional/J11CookbkCh4_Functional.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j11cookbkch4_functional;
+
+/**
+ *
+ * @author ngke
+ */
+public class J11CookbkCh4_Functional {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
